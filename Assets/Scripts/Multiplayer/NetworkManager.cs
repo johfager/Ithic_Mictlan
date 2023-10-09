ca449184889c075bbf26d6a2e1738826787119cb
AudibleParrot2 <ricardonunezalanis@live.com.mx>
2023-10-09 17:30:26 -0600

    Connection and creation of a room in a single button

---
+++ Assets/Scripts/Multiplayer/NetworkManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Connect();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Connect()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public void Play()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Failed");
+        //Failed because no room in existance
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 4 });
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Success");
+    }
+}
