0e66988c591f2df33e0dac6080907dbaf4dd243d
coolestKitten <sepcaro2@gmail.com>
2023-10-14 12:45:42 -0600

    added item interface script

---
+++ Assets/Scripts/ItemManagement/Item.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+    // internal id of the item 
+    public int id;
+
+    // name of the item which the player will see 
+    public string name;
+
+    // 1 - common
+    // 2 - rare
+    // 3 - very rare
+    public int rarity; 
+
+    // short description of the item and its effect
+    public string description;
+}
